54096e15def5a695cfcad1010ea60b8a527c6a2b
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-26 09:29:32 +0000

    restructured

--- start.ps1
+++ start.ps1
@@ -2,6 +2,6 @@ docker.exe run --rm -it `
   -v ${HOME}/.ssh:/tmp/.ssh:ro `
   -v ${HOME}/.gitconfig:/root/.gitconfig `
   -v ${HOME}/projects:/root/projects `
-  -w /root/projects `
+  -w /root/projects/DockerFiles/debian-box `
   my-debian-box `
   /bin/bash `
