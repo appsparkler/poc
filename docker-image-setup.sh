34d713517e8eb7f281069d1d9f115f49ee41a870
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-27 07:47:45 +0000

    moved software installation and apt update/upgrade tasks to image-setup-file

---
+++ docker-image-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e;
+# chmod +x /bin/docker-entrypoint.sh;
+
+# APT tasks
+apt-get update;
+apt-get -y upgrade;
+apt-get install -y apt-utils;
+apt-get install -y nano;
+
+exec bash;
