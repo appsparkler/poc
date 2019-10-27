08283d7d11c894304698b7387c772a33f47e8c24
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-27 06:16:36 +0000

    updated start-script

--- start.ps1
+++ start.ps1
@@ -1,7 +1,9 @@
+docker.exe pull appsparkler/debian-box-base:latest;
+
 docker.exe run --rm -it `
   -v ${HOME}/.ssh:/tmp/.ssh:ro `
   -v ${HOME}/.gitconfig:/root/.gitconfig `
   -v ${HOME}/projects:/root/projects `
   -w /root/projects/DockerFiles/debian-box `
-  my-debian-box `
-  /bin/bash `
+  appsparkler/debian-box-base `
+  /bin/bash `;
