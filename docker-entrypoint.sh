16c69402a8dba8b37063ad4411f94d3f3fbb722d
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-27 07:46:56 +0000

    removed unnecessary entrypoint tasks - these should  be under image-setup-tasks. Entrypoint-script is executed when the image is in or mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,4 @@
-#!/usr/bin/env bash
+#! /bin/bash
 set -e;
-
-chmod +x /bin/docker-entrypoint.sh;
-
-# APT tasks
-apt-get update;
-apt-get -y upgrade;
-apt-get install -y apt-utils;
-apt-get install -y nano;
-
-# 
-
+cp -r /tmp/.ssh /root;
 exec bash;
