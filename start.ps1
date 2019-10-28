89501d3a8147bb129ca295eaa0fa10d486a0b204
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-28 06:58:19 +0000

    updated ps1 script for new image

--- start.ps1
+++ start.ps1
@@ -1,9 +1,9 @@
-docker.exe pull appsparkler/debian-box-base:latest;
+docker.exe pull appsparkler/my-debian-box:latest;
 
 docker.exe run --rm -it `
   -v ${HOME}/.ssh:/tmp/.ssh:ro `
   -v ${HOME}/.gitconfig:/root/.gitconfig `
   -v ${HOME}/projects:/root/projects `
-  -w /root/projects/DockerFiles/debian-box `
+  -w /root/projects/github/pocpin `
   appsparkler/debian-box-base `
   /bin/bash `;
