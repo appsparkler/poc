8ddca78f496ce66c97056c2ad678ef1ef5eba801
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-28 06:44:41 +0000

    removed ps1 file as we will not be running a container with this image

--- start.ps1
+++
@@ -1,9 +0,0 @@
-docker.exe pull appsparkler/debian-box-base:latest;
-
-docker.exe run --rm -it `
-  -v ${HOME}/.ssh:/tmp/.ssh:ro `
-  -v ${HOME}/.gitconfig:/root/.gitconfig `
-  -v ${HOME}/projects:/root/projects `
-  -w /root/projects/DockerFiles/debian-box `
-  appsparkler/debian-box-base `
-  /bin/bash `;
