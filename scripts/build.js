67d09c71a5f2f914e6a6c237cce2df0b7eba3efe
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-28 10:01:10 +0000

    updated build scripts--

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,10 @@
+const webpack = require('webpack')
 const webpackConfig = require(`${process.env.INIT_CWD}/config/webpack/common`)
+const compiler = webpack(webpackConfig)
+compiler.run(compilerCB)
+
+function compilerCB() {
+  console.log(arguments) // eslint-disable-line
+}
 
 console.log(webpackConfig) // eslint-disable-line
