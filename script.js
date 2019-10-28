938aa3634d17bea344b55e7eb57c02844995eac6
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-28 05:47:13 +0000

    restructured script.js to make it more semantic

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var x = "hello world";
-
-decideToCook(x);
-buyGroceries();
-cook();
-
-function decideToCook() {
+export default resolveHunger;
+// module.exports = resolveHunger;
+// abstractions
+function resolveHunger(whatsOnTheMenu) {
+  planToCook(whatsOnTheMenu);
+  buyGroceries(whatsOnTheMenu);
+  cook(whatsOnTheMenu);
+  eat(whatsOnTheMenu);
+}
+function eat() {
+  //
+}
+function planToCook() {
   //
 }
 function buyGroceries() {
