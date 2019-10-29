59a3c668c9d7181af9e8924839eb49d9c769fef0
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-29 18:40:53 +0530

    prefetching with webpack in src code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-// import _ from 'lodash'
-import(/* webpackChunkName: "lodash" */ 'lodash').then(function({default: _}) {
-  console.log(_)
-  const elem = document.createElement('h2')
-  elem.innerHTML = _.join(['Hello', 'Webpack'], ' ')
-  document.body.appendChild(elem)
-})
+for(let i = 0; i < 10000; i++) {
+  console.log(10 + i + 20)
+} //eslint-disable-line
 
-
-// console.log('hello world...')
-// getComponent().then(function(component) {
-//   document.appendChild(component)
-// })
-// function getComponent() {
-//   import(/* webpackChunkName: "lodash" */ 'lodash''lodash')
-//     .then({ default: _ } => {
-//       const elem = document.createElement('div');
-//       elem.innerHTML = _.join(['Hello', 'Webpack'], ' ')
-//     });
-// }
+import(/*webpackPrefetch: true*/'lodash');
