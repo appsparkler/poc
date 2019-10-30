1d0f26b023ef5edbe80aa22d307a18c1cc62df7b
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-30 09:10:15 +0530

    working with function-literal instead of arrow functions

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import _ from 'lodash'
 function component() {
   const elem = document.createElement('div')
   elem.innerHTML = _.join(['Hello', 'Webpack'], ' ')
-  elem.onclick = () => import('./print')
-    .then(print)
+  elem.onclick = print
   return elem
 }
 
 document.body.appendChild(component())
 
-function print({default: Print}) {
-  Print('Hello Webpack!')
+// function print({default: Print}) {
+//   Print('Hello Webpack!')
+// }
+
+function print() {
+  import('./print')
+    .then(({default: Print}) =>{
+      Print('Hello Webpack!')
+    })
 }
