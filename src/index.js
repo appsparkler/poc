5c5e7f2fa4f569eef43942a77c3da6dbd631c9ea
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-31 12:34:14 +0530

    working loading-data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
+import MyData from './data.xml'
 
 function component() {
   const element = document.createElement('div')
 
   // Lodash, currently included via a script, is required for this line to work
   element.classList.add('hello')
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = _.join(['Hello', 'webpack', JSON.stringify(MyData)], ' ')
 
   // new image
   const myIcon = new Image()
