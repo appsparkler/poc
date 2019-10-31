3c7aace874effc59438626ba10b60e01db08468e
Aakash Shah <aakash.shah@publicissapient.com>
2019-10-31 10:49:14 +0530

    added file-loader for images to webpack-config, updated style.css to include the image and updated index.js to import the image

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
           'style-loader',
           'css-loader',
         ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
