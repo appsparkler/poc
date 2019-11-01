01127b0e850a30f1fb78897722050664e91c83aa
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-01 06:43:55 +0530

    different way of setting up of pre-loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
+import printMe from './print'
+import('lodash' /* webpackPrefetch: true, webpackChunkName: 'lodash' */)
 
-const btn = document.createElement('button')
-renderLoadingIndicator()
-renderElement(btn)
-import('./print' /* webpackPrefetch: true */)
-  .then(setEventHandlerOnButton.bind(null, btn))
+renderElement()
 
-function renderElement(btn) {
-  // alert('rendering element now...')
-  document.getElementById('loading-indicator').style.display = 'none'
+function renderElement() {
+  const btn = document.createElement('button')
   const element = document.createElement('div')
   // setup the div
   element.classList.add('hello')
@@ -15,23 +12,10 @@ function renderElement(btn) {
     <h1>Hello Webpack</h1>
     <br>
   `
-
   // setup the button
-  btn.disabled = true
   btn.innerHTML = `User is expected to click this button at a later time...`
+  btn.onclick = printMe
   //
   element.appendChild(btn)
   document.body.appendChild(element)
-}
-
-
-function setEventHandlerOnButton(btn, {default: printMe}) {
-  btn.onclick = printMe
-  btn.disabled = false
-}
-
-function renderLoadingIndicator() {
-  const div = document.createElement('div')
-  div.innerHTML = '<h1 id="loading-indicator">Loading...</h1>'
-  document.body.appendChild(div)
 }
--- src/print.js
+++ src/print.js
@@ -1,5 +1,9 @@
-import _ from 'lodash'
 
 export default function printInAlertBox() {
+  import('lodash')
+    .then(showAlertBox)
+}
+
+function showAlertBox({ default: _ }) {
   alert(_.join(['I get called', 'from print.js!!!!!!'], ' '))
 }
