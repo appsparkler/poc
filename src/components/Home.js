534708fe577aafedca09d1217b450427fe274bf2
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-01 18:57:25 +0530

    functional approact to components

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,45 +6,47 @@ class Home extends Component {
   state = {
     posts: []
   }
-
-  componentDidMount() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        // alert(JSON.stringify())
-        const randomNumber = Math.floor(Math.random() * 80)
-        // alert(res.data.length);
-        this.setState({
-          posts: res.data.splice(randomNumber, 10)
-        })
-      })
+  async componentDidMount() {
+    await setPostDataOnState.call(this)
   }
   render() {
-    const { posts } = this.state
-    const noPostsJSX = (
-      <div>No Posts Yet...</div>
-    )
-    const map_postsList = post => (
-      <div className="post card" key={post.id}>
-        <div className='card-content'>
-          <Link to={{pathname:`/${post.id}`, state: post}}  >
-            <h1 className='card-title red-text'>{post.title}</h1>
-          </Link>
-          <p>
-            { post.body.substr(0, 80) }...
-          </p>
-        </div>
-      </div>
-    )
-    const postsList = posts && posts.length ?
-      posts.map(map_postsList) : noPostsJSX
-    return (
-      <div className="container">
-        <h4 className="center red-text">Home</h4>
-        { postsList }
-      </div>
-    )
+    return getComponentJSX.call(this)
   }
 }
 
 export default Home
+
+async function setPostDataOnState() {
+  const posts = await axios.get('https://jsonplaceholder.typicode.com/posts')
+  const randomNumber = Math.floor(Math.random() * 80)
+  this.setState({
+    posts: posts.data.splice(randomNumber, 10)
+  })
+}
+
+function getComponentJSX() {
+  const { posts } = this.state
+  const noPostsJSX = (
+    <div>No Posts Yet...</div>
+  )
+  const map_postsList = post => (
+    <div className="post card" key={post.id}>
+      <div className='card-content'>
+        <Link to={{pathname:`/${post.id}`, state: post}}  >
+          <h1 className='card-title red-text'>{post.title}</h1>
+        </Link>
+        <p>
+          { post.body.substr(0, 80) }...
+        </p>
+      </div>
+    </div>
+  )
+  const postsList = posts && posts.length ?
+    posts.map(map_postsList) : noPostsJSX
+  return (
+    <div className="container">
+      <h4 className="center red-text">Home</h4>
+      { postsList }
+    </div>
+  )
+}
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
-
 
 export default class Post extends Component {
   state = {
@@ -37,32 +35,3 @@ async function setPostDataOnState() {
     post: post.data
   })
 }
-
-// export default function(props) {
-//   let post  = props.location.state && props.location.state.id ?
-//     props.location.state: undefined
-//   if(!post) {
-//     console.log('fetching post...')
-//     post = axios.get('https://jsonplaceholder.typicode.com/posts/1')
-//       .then(remotePost => {
-//         post = remotePost
-//       })
-//       console.log(post)
-//   }
-//   if (post) {
-//     return(
-//       <div className="container">
-//         <div className="post card">
-//           <div className='card-content'>
-//             <h1 className='card-title red-text'>{post.title}</h1>
-//             <p>
-//               { post.body }
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   } else {
-//     return <h1 className="container">No Posts available, Please go to <Link to="/">Home Page</Link></h1>
-//   }
-// }
