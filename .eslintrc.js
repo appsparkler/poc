44501dbe39556913a2f55e9d7a155f35f6aa9e80
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-04 17:47:03 +0530

    resolving eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,30 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
+    "jest/globals": true
   },
   extends: [
     'airbnb',
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
   plugins: [
     'react',
+    'jest'
   ],
   rules: {
-  },
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
+  }
 };
