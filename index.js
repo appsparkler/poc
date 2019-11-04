8e6c02acd137497543bfbdb3fcac5993b3239bd1
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-04 15:40:56 +0530

    SUPER MODULARIZATION

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const Hapi = require('@hapi/hapi')
-const HelloWorldPlugin = require('./plugins/hello-world').plugin
+const HelloWorldPlugin = require('./plugins/hello-world')
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
-  })
+async function registerHelloWorldPluginOnServer(server) {
   await server.register({
     plugin: HelloWorldPlugin,
     options: {
-      msg: 'Hey! Wassup',
+      msg: 'Hey! Wassup?',
     },
   })
+}
+
+async function startServer(server) {
   await server.start()
   console.log(`Server is running on ${server.info.uri}`)
 }
 
-process.on('unhandedRejection', (err) => {
-  console.log(err)
-  process.exit(1)
-})
+function setupServer() {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  })
+  return server
+}
+
+function handleUnhandledRejections() {
+  process.on('unhandedRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+  })
+}
+
+async function init() {
+  const server = setupServer()
+  await registerHelloWorldPluginOnServer.call(this, server)
+  await startServer.call(this, server)
+}
+
+handleUnhandledRejections()
 
 init()
