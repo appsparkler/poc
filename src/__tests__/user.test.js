ae1a9cf06b85a21434d5a910fe2b6c3afaa0eb2d
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-05 15:40:12 +0530

    WITH RESOLVES

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -5,3 +5,8 @@ it('works with promises', () => {
   expect.assertions(1)
   return user.getUserName(4).then((data) => expect(data).toEqual('Mark'))
 })
+
+it('works with resolves', () => {
+  expect.assertions(1)
+  return expect(user.getUserName(5)).resolves.toEqual('Paul')
+})
