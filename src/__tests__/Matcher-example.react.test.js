4528606eb37b21fa905ff1b0087988245cbfb3b4
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-05 13:59:57 +0530

    PROPERTY MATCHERS- PASSING TEST

--- src/__tests__/Matcher-example.react.test.js
+++ src/__tests__/Matcher-example.react.test.js
@@ -4,5 +4,17 @@ test('it will fail everytime!', () => {
     id: Math.floor(Math.random() * 20),
     name: 'LeBron James',
   }
-  expect(user).toMatchInlineSnapshot()
+  expect(user).toMatchInlineSnapshot(
+      {
+        createdAt: expect.any(Date),
+        id: expect.any(Number),
+      },
+      `
+    Object {
+      "createdAt": Any<Date>,
+      "id": Any<Number>,
+      "name": "LeBron James",
+    }
+  `,
+  )
 })
