a0135dee5ee230fcfb488f20ca0b337daedddaab
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-05 18:07:28 +0530

    RUNNING ALL TIMERS

--- src/__tests__/timerGame.js
+++ src/__tests__/timerGame.js
@@ -7,3 +7,23 @@ test('wait 1 second before ending the game', () => {
   expect(setTimeout).toHaveBeenCalledTimes(1)
   expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000)
 })
+
+/*
+  Another test we might want to write for this module is one that asserts
+  that the callback is called after 1 second. To do this, we're going to
+  use Jest's timer * control APIs to fast-forward time right in the middle
+  of the test:
+*/
+test('calls the callback after 1 second', () => {
+  const timerGame = require('../timerGame')
+  const callback = jest.fn()
+  timerGame(callback)
+
+  expect(callback).not.toBeCalled()
+
+  // Fast-forward until all timers have been executed
+  jest.runAllTimers()
+
+  expect(callback).toBeCalled()
+  expect(callback).toHaveBeenCalledTimes(1)
+})
