5ae2e067f3b9e6018f53f300138b9abf9f884a83
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-05 13:33:01 +0530

    INLINE SNAPSHOTS

--- src/__tests__/Link.react.test.js
+++ src/__tests__/Link.react.test.js
@@ -2,9 +2,16 @@ import React from 'react'
 import Link from '../Link.react.js'
 import renderer from 'react-test-renderer'
 
-it('renders correctly', ()=> {
+it('renders correctly', () => {
   const tree = renderer
       .create(<Link page="https://www.instagram.com">Instagram</Link>)
       .toJSON()
-  expect(tree).toMatchSnapshot()
+  expect(tree).toMatchInlineSnapshot(`
+    <a
+      className="normal"
+      href="https://www.instagram.com"
+    >
+      Instagram
+    </a>
+  `)
 })
