3828569ad538049ed27428b4f792d16bdb8b500b
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-06 10:01:31 +0530

    - Basic Navbar created

--- src/components/layout/Navbar.react.js
+++ src/components/layout/Navbar.react.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import {Link, BrowserRouter as Router} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className="navbar-wrapper gray darken-3">
       <div className="container">
-        <Router>
-          <Link to="/" className="brand-logo">
+        <Link to="/" className="brand-logo">
             MarioPlan
-          </Link>
-        </Router>
+        </Link>
       </div>
     </div>
   )
--- src/components/layout/__tests__/Navbar.react.test.js
+++ src/components/layout/__tests__/Navbar.react.test.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../Navbar.react'
 import renderer from 'react-test-renderer'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 it('should render the navbar correctly', () => {
-  const component = renderer.create(<Navbar />)
+  const component = renderer.create(
+      <Router>
+        <Navbar />
+      </Router>
+  )
   const tree = component.toJSON()
+  expect.assertions(1)
   expect(tree).toMatchInlineSnapshot(`
     <div
       className="navbar-wrapper gray darken-3"
