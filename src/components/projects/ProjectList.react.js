d2f3ae178ed18ac7824f8bda94914304edbe660c
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-06 12:24:31 +0530

    Setup ProjectSummary component

--- src/components/projects/ProjectList.react.js
+++ src/components/projects/ProjectList.react.js
@@ -1,39 +1,12 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary.react'
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-
-      <div className="project-summary card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Net Ninja</p>
-          <p className="grey-text">
-            3rd September, 2am
-          </p>
-        </div>
-      </div>
-
-      <div className="project-summary card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Net Ninja</p>
-          <p className="grey-text">
-            3rd September, 2am
-          </p>
-        </div>
-      </div>
-
-      <div className="project-summary card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted By Net Ninja</p>
-          <p className="grey-text">
-            3rd September, 2am
-          </p>
-        </div>
-      </div>
-
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
