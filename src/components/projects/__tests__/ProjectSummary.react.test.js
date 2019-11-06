9f62cd5316f5e3cb33a735ecc8da0f7e4e07fe8b
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-06 12:27:43 +0530

    - corrected import statement - including generated snapshots

--- src/components/projects/__tests__/ProjectSummary.react.test.js
+++ src/components/projects/__tests__/ProjectSummary.react.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjectSummary from '../ProjectSummary.react'
-import renderer from 'react-rest-renderer'
+import renderer from 'react-test-renderer'
 
 it('should render correctly', () => {
   expect.assertions(1)
