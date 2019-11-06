f1ea980271eb69067d5e67c8706f721c42ac240a
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-06 11:13:22 +0530

    PASSING TESTS - Dashboard container

---
+++ src/components/dashboard/Dashboard.react.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return getComponentJSX.call(this)
+  }
+}
+
+export default Dashboard
+
+// abstractions
+function getComponentJSX() {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6">
+          Project List
+        </div>
+        <div className="col s12 m5 offset-m1">
+          Notifications
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/dashboard/__tests__/Dashboard.react.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Dashboard from '../Dashboard.react'
+
+it('should render as expected', () => {
+  const component = renderer.create(<Dashboard />)
+  const tree = component.toJSON()
+  expect.assertions(1)
+  expect(tree).toMatchInlineSnapshot(`
+    <div
+      className="dashboard container"
+    >
+      <div
+        className="row"
+      >
+        <div
+          className="col s12 m6"
+        >
+          Project List
+        </div>
+        <div
+          className="col s12 m5 offset-m1"
+        >
+          Notifications
+        </div>
+      </div>
+    </div>
+  `)
+})
