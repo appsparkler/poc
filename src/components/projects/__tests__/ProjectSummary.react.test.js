f7412255a7a89bf6089f722c254ec1bca97f9009
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-08 08:54:47 +0530

    ProjectSummary - - passing tests - 100% coverage

--- src/components/projects/__tests__/ProjectSummary.react.test.js
+++ src/components/projects/__tests__/ProjectSummary.react.test.js
@@ -4,7 +4,14 @@ import renderer from 'react-test-renderer'
 
 it('should render correctly', () => {
   expect.assertions(1)
-  const component = renderer.create(<ProjectSummary />)
+  const props = {
+    project: {
+      id: '1',
+      title: 'Hello World',
+      content: 'This is project content written in plain text...',
+    },
+  }
+  const component = renderer.create(<ProjectSummary {...props} />)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
