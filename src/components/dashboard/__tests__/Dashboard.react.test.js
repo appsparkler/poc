047166191a136920acb96939429c3e45e6510d94
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-08 11:10:26 +0530

    added a mock-store, - improved coverage

--- src/components/dashboard/__tests__/Dashboard.react.test.js
+++ src/components/dashboard/__tests__/Dashboard.react.test.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import {Dashboard} from '../Dashboard.react'
+import Dashboard from '../Dashboard.react'
+import configureMockStore from 'redux-mock-store'
 
 it('should render as expected', () => {
+  const mockStore = configureMockStore()
+  const store = mockStore({
+    project: {
+      projects: [{
+        id: '1',
+        title: 'Title 1',
+        content: 'Content...',
+      }],
+    },
+  })
   const component = renderer.create(
-      <Dashboard />
+      <Dashboard store={store}/>
   )
   const tree = component.toJSON()
   expect.assertions(1)
