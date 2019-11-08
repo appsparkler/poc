ac7758f0fe6c28972f3dcb4219254603ad6156b7
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-08 06:32:47 +0530

    store setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min'
 import './index.css'
 import App from './App.react.js'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const AppJSX = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(AppJSX, document.getElementById('root'))
