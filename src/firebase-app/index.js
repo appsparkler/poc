abb4b30596a8aac824069ff7ecc859e188742a0c
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-09 11:34:42 +0530

    - added firebase module - setup the db

---
+++ src/firebase-app/index.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyCEe-9P0ugfD-KUPTz5TbBEVE6zGM5Zl3M',
+  authDomain: 'the-marioplan.firebaseapp.com',
+  databaseURL: 'https://the-marioplan.firebaseio.com',
+  projectId: 'the-marioplan',
+  storageBucket: 'the-marioplan.appspot.com',
+  messagingSenderId: '118979864382',
+  appId: '1:118979864382:web:67fb4e68d30e14dad66e6f',
+}
+
+firebase.initializeApp(config)
+
+const db = firebase.firestore()
+
+export default {db}
