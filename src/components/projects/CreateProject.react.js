e4978bb69326683dee43401eeeabab325f115ed7
Aakash Shah <aakash.shah@publicissapient.com>
2019-11-09 09:38:34 +0530

    WIP (before switching to the react-redux-examples

--- src/components/projects/CreateProject.react.js
+++ src/components/projects/CreateProject.react.js
@@ -1,63 +1,104 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {createProject} from '../../store/actions/project'
+import React, {useState} from 'react'
+import {useFirebase} from 'react-redux-firebase'
 import cardify from '../materialize/Cardify.react'
 import containerify from '../materialize/Containerify.react'
 
-export class CreateProject extends Component {
-  constructor() {
-    super()
-    this.state = {
-      title: '',
-      content: '',
-    }
-  }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    e.stopPropagation()
-    this.props.createProject(this.state)
-  }
+// import {connect} from 'react-redux'
+// import {createProject} from '../../store/actions/project'
 
-  handleChange(e) {
-    this.setState({
-      [e.target.id]: e.target.value,
-    })
-  }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)} className="white">
-        <h4 className="grey-text text-darken-3">Create new project</h4>
-        <div className="input-field">
-          <label htmlFor="title">
-            Title
-          </label>
-          <input
-            id="title"
-            type="text"
-            onChange={this.handleChange.bind(this)} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="content">
-            Content
-          </label>
-          <textarea
-            className="materialize-textarea"
-            id="content"
-            onChange={this.handleChange.bind(this)}
-          ></textarea>
-        </div>
-        <div className="input-field">
-          <button className="btn pink lighten-1 z-depth-0" type="submit">
-            Create
-          </button>
-        </div>
-      </form>
-    )
+function CreateProject() {
+  const firebase = useFirebase()
+  const [title, changeTitle] = useState('')
+  const [content, changeContent] = useState('')
+  const project = {
+    title, changeTitle,
+    content, changeContent,
+  }
+  // const [inputVal, changeInput] = useState('')
+  return getJSX.call(null, {
+    handleChange: handleChange.bind(null, project),
+    handleSubmit: handleSubmit.bind(null, project, firebase),
+  })
+  // return (<div>Hello World</div>)
+}
+// export class CreateProject extends Component {
+// constructor() {
+//   super()
+//   this.state = {
+//     title: '',
+//     content: '',
+//   }
+// }
+
+//   handleSubmit(e) {
+//     const firebase = useFirebase()
+//     console.log(firebase)
+//     e.preventDefault()
+//     e.stopPropagation()
+//     this.props.createProject(this.state)
+//     // firebase.push('projects', {
+//     //   ...this.state,
+//     //   authorFirstName: 'Akash',
+//     //   authorLastname: 'Shah',
+//     // })
+//   }
+//
+//   return getJSX.call({handleChange, handleSubmit})
+// }
+
+function handleSubmit({title, content}, firebase, e) {
+  e.preventDefault()
+  e.stopPropagation()
+  firebase.push('projects', {
+    title,
+    content,
+    authorFirstName: 'Akash',
+    authorLastname: 'Shah',
+  })
+}
+
+function handleChange(project, e) {
+  if (e && e.target && e.target.id === 'title') {
+    return project.changeTitle(e && e.target && e.target.value)
+  } else if (e && e.target && e.target.id === 'content') {
+    return project.changeContent(e && e.target && e.target.value)
   }
 }
 
-export default containerify(cardify(
-    connect(null, {createProject})(CreateProject)
-))
+function getJSX({handleSubmit, handleChange}) {
+  return (
+    <form onSubmit={handleSubmit} className="white">
+      <h4 className="grey-text text-darken-3">Create new project</h4>
+      <div className="input-field">
+        <label htmlFor="title">
+    Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          onChange={handleChange} />
+      </div>
+      <div className="input-field">
+        <label htmlFor="content">
+          Content
+        </label>
+        <textarea
+          className="materialize-textarea"
+          id="content"
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <div className="input-field">
+        <button
+          className="btn pink lighten-1 z-depth-0"
+          type="submit">
+          Create
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default containerify(cardify(CreateProject))
