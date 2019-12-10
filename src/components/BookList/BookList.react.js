68b8e0a3eabe9181ca241681a88b16f1f1ab6c61
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-10 14:10:31 +0530

    - Added Navbar and BookList component - App initial setup

---
+++ src/components/BookList/BookList.react.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul class="list-group mt-2">
+        <li class="list-group-item">The Way Of The Kings</li>
+        <li class="list-group-item">The Name Of The Wind</li>
+        <li class="list-group-item">The Final Empire</li>
+      </ul>
+    );
+  }
+}
+
+export default BookList;
---
+++ src/components/Navbar/Navbar.react.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">Context App</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
