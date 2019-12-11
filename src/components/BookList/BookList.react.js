8b41576c3cad6aafd8743200993b8f38dc2124ad
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-11 09:24:16 +0530

    - An Alternate Approach to consument contextual-data

--- src/components/BookList/BookList.react.js
+++ src/components/BookList/BookList.react.js
@@ -2,18 +2,30 @@ import React, {Component} from 'react'
 import {ThemeContext} from '../../context/ThemeContext.react'
 
 class BookList extends Component {
-  static contextType = ThemeContext
   render() {
-    const {theme} = this.context
     return (
-      <ul className={`list-group rounded-0 mt-2`}>
-        <li className={`list-group-item rounded-0 list-group-item-${theme}`}>
-          The Way Of The Kings</li>
-        <li className={`list-group-item rounded-0 list-group-item-${theme}`}>
-        The Name Of The Wind</li>
-        <li className={`list-group-item rounded-0 list-group-item-${theme}`}>
-          The Final Empire</li>
-      </ul>
+      <ThemeContext.Consumer>
+        {
+          ({theme})=>(
+            <ul className={`list-group rounded-0 mt-2`}>
+              <li className={`list-group-item rounded-0
+                    list-group-item-${theme}`
+              }>
+                The Way Of The Kings</li>
+              <li className={`list-group-item rounded-0
+                list-group-item-${theme}`
+              }>
+                The Name Of The Wind</li>
+              <li className={
+                `list-group-item rounded-0
+                list-group-item-${theme}`
+              }>
+                The Final Empire
+              </li>
+            </ul>
+          )
+        }
+      </ThemeContext.Consumer>
     )
   }
 }
--- src/components/Navbar/Navbar.react.js
+++ src/components/Navbar/Navbar.react.js
@@ -2,28 +2,34 @@ import React, {Component} from 'react'
 import {ThemeContext} from '../../context/ThemeContext.react'
 
 class Navbar extends Component {
-  static contextType = ThemeContext
   render() {
-    const {theme} = this.context
     return (
-      <nav
-        className={`navbar navbar-expand-lg navbar-${theme} bg-${theme} mt-2`}
-      >
-        <a className="navbar-brand" href="/">Context App</a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/home">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <ThemeContext.Consumer>
+        {({theme}) => (
+          <nav
+            className={
+              `navbar navbar-expand-lg
+                navbar-${theme} bg-${theme}
+                mt-2`
+            }
+          >
+            <a className="navbar-brand" href="/">Context App</a>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="/home">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        )}
+      </ThemeContext.Consumer>
     )
   }
 }
