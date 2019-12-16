3e8273c3bd7c8a8c6c4df21bd16ca1d4e617cdb0
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-16 09:09:03 +0530

    - :whale: Working with react-hook useContext instead of Context.Provider in class Components

--- src/components/Navbar/Navbar.react.js
+++ src/components/Navbar/Navbar.react.js
@@ -1,46 +1,37 @@
-import React, {Component} from 'react'
+import React, {useContext} from 'react'
 import {ThemeContext} from '../../context/ThemeContext.react'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {({theme}) => (
-          <nav
-            className={
-              `navbar navbar-expand-lg
-                navbar-${theme} bg-${theme}
-                mt-2
-                border border-primary`
-            }
-          >
-            <a className="navbar-brand" href="/">
-              Context App
-            </a>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="/home">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/contact">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        )}
-      </ThemeContext.Consumer>
-    )
-  }
+export default function Navbar() {
+  const {theme} = useContext(ThemeContext)
+  return (<nav
+    className={
+      `navbar navbar-expand-lg
+        navbar-${theme} bg-${theme}
+        mt-2
+        border border-primary`
+    }
+  >
+    <a className="navbar-brand" href="/">
+      Context App
+    </a>
+    <div className="collapse navbar-collapse">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="/home">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>)
 }
-
-export default Navbar
