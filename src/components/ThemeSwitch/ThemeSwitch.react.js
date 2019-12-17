70188b00d65d5cd9584a2941442cba63bee717d7
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-17 09:28:52 +0530

    - :whale: converting a component from Class-Component to Stateless Component with React Hooks

--- src/components/ThemeSwitch/ThemeSwitch.react.js
+++ src/components/ThemeSwitch/ThemeSwitch.react.js
@@ -1,46 +1,38 @@
-import React, {Component} from 'react'
+import React, {useContext} from 'react'
 import {ThemeContext} from '../../context/ThemeContext.react'
 
-class ThemeSwitch extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {
-          ({toggleTheme}) => (
-            <form onChange={toggleTheme}>
-              <div
-                className="btn-group btn-group-toggle rounded-0 border \n
-                border-primary mt-2"
-                data-toggle="buttons"
-                title="Select Theme">
-                <label className="btn btn-dark active rounded-0">
-                  <input
-                    type="radio"
-                    name="theme"
-                    autoComplete="off"
-                    value="dark"
-                    onClick={toggleTheme}
-                  />
-                    Dark
-                </label>
-                <label className="btn btn-light rounded-0">
-                  <input
-                    type="radio"
-                    name="theme"
-                    autoComplete="off"
-                    value="light"
-                    onClick={toggleTheme}
-                  />
-                    Light
-                </label>
-              </div>
-            </form>
-          )
-        }
-
-      </ThemeContext.Consumer>
-    )
-  }
+function ThemeSwitch() {
+  const {toggleTheme} = useContext(ThemeContext)
+  return (
+    <form onChange={toggleTheme}>
+      <div
+        className="btn-group btn-group-toggle rounded-0 border \n
+        border-primary mt-2"
+        data-toggle="buttons"
+        title="Select Theme">
+        <label className="btn btn-dark active rounded-0">
+          <input
+            type="radio"
+            name="theme"
+            autoComplete="off"
+            value="dark"
+            onClick={toggleTheme}
+          />
+            Dark
+        </label>
+        <label className="btn btn-light rounded-0">
+          <input
+            type="radio"
+            name="theme"
+            autoComplete="off"
+            value="light"
+            onClick={toggleTheme}
+          />
+            Light
+        </label>
+      </div>
+    </form>
+  )
 }
 
 export default ThemeSwitch
