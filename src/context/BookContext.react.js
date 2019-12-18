e4ea9ad2560d4fbfd8fc79153aa68142a607572b
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-18 09:18:34 +0530

    :whale: Created a Contex with functional component

---
+++ src/context/BookContext.react.js
@@ -0,0 +1,36 @@
+import React, {createContext, useState} from 'react'
+import uuid from 'uuid/v1'
+
+export const BookContext = createContext()
+
+function addBook(books, setBooks, newBook) {
+  setBooks({
+    ...books,
+    newBook,
+  })
+}
+
+export default function BookContextProvider({children}) {
+  const INITIAL_BOOKS = [
+    {
+      title: 'The Final Empire',
+      id: uuid(),
+    }, {
+      title: 'Name of the Wind',
+      id: uuid(),
+    }, {
+      title: 'The Hero Of Ages',
+      id: uuid(),
+    }, {
+      title: 'The Way Of Kings',
+      id: uuid(),
+    },
+  ]
+  const [books, setBooks] = useState(INITIAL_BOOKS)
+  return (<BookContext.Provider value={{
+    books,
+    addBook: addBook.bind(null, books, setBooks),
+  }}>
+    {children}
+  </BookContext.Provider>)
+}
