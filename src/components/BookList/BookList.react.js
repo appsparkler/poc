326e683d99e9b5ccc0bd4fc041ccacc8169eb0c3
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-18 09:35:03 +0530

    :fire: removed dev comments

--- src/components/BookList/BookList.react.js
+++ src/components/BookList/BookList.react.js
@@ -4,9 +4,6 @@ import {BookContext} from '../../context/BookContext.react'
 
 export default function BookList() {
   const {books} = useContext(BookContext)
-  // const books = [
-  //   {id: 1, title: 'hey...'},
-  // ]
   const {theme} = useContext(ThemeContext)
   return (
     <div className="p-2 mt-2 border border-primary">
@@ -21,36 +18,3 @@ export default function BookList() {
     </div>
   )
 }
-
-//
-// class BookList extends Component {
-//   render() {
-//     return (
-//       <ThemeContext.Consumer>
-//         {
-//           ({theme})=>(
-//             <ul
-// className={`list-group rounded-0 mt-2 border border-primary`}>
-//               <li className={`list-group-item rounded-0
-//                     list-group-item-${theme}`
-//               }>
-//                 The Way Of The Kings</li>
-//               <li className={`list-group-item rounded-0
-//                 list-group-item-${theme}`
-//               }>
-//                 The Name Of The Wind</li>
-//               <li className={
-//                 `list-group-item rounded-0
-//                 list-group-item-${theme}`
-//               }>
-//                 The Final Empire
-//               </li>
-//             </ul>
-//           )
-//         }
-//       </ThemeContext.Consumer>
-//     )
-//   }
-// }
-
-// export default BookList
