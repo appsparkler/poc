d301e958ca49b3289af7d8832c6be3708d30f833
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-19 17:52:54 +0530

    :whale: setup movie context

---
+++ src/context/MovieContext.react.js
@@ -0,0 +1,32 @@
+import React, {createContext, useState} from 'react'
+import uuid from 'uuid/v1'
+
+const MovieContext = createContext()
+
+function addMovieBinded(movies, setMovies, newMovie) {
+  newMovie = Object.assign(newMovie, {
+    uuid: uuid(),
+  })
+  setMovies([
+    ...movies,
+    newMovie,
+  ])
+}
+
+function removeMovieBinded(movies, setMovies, movie2Remove) {
+  setMovies(movies.filter((movie) => movie.uuid !== movie2Remove.uuid ))
+}
+
+export default function MovieContextProvider({children}) {
+  const [movies, setMovies] = useState([])
+  const addMovie = addMovieBinded.bind(null, movies, setMovies)
+  const removeMovie = removeMovieBinded.bind(null, movies, setMovies)
+  const context = {
+    movies,
+    addMovie,
+    removeMovie,
+  }
+  return (<MovieContext.Provider value={context}>
+    {children}
+  </MovieContext.Provider>)
+}
