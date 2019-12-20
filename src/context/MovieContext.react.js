b80f8fc863faff543f85bb1cfdd1be9ccc499dcf
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-20 17:03:45 +0530

    - :whale: - Working with useEffect to store data in localStorage everytime a movies-state is updated

--- src/context/MovieContext.react.js
+++ src/context/MovieContext.react.js
@@ -1,14 +1,18 @@
-import React, {createContext, useReducer} from 'react'
+import React, {createContext, useReducer, useEffect} from 'react'
 import MovieReducer from '../reducers/MovieReducer.react'
 
 export const MovieContext = createContext()
 
 export default function MovieContextProvider({children}) {
-  const [movies, dispatch] = useReducer(MovieReducer, [])
+  const localData = JSON.parse(localStorage.getItem('movies')) || []
+  const [movies, dispatch] = useReducer(MovieReducer, localData)
   const context = {
     movies,
     dispatch,
   }
+  useEffect(() => {
+    localStorage.setItem('movies', JSON.stringify(movies))
+  }, [movies])
   return (<MovieContext.Provider value={context}>
     {children}
   </MovieContext.Provider>)
