166bb128ec05c03ea1b22ccdaf0219b3542d0513
Aakash Shah <aakash.shah@publicissapient.com>
2019-12-28 15:53:46 +0530

    updated styles

--- src/components/projects/ProjectDetails.react.js
+++ src/components/projects/ProjectDetails.react.js
@@ -12,26 +12,37 @@ const ProjectDetails = (props) => {
   return (
     <>
       <div className="project-details section container">
-        <div className="card z-depth-0">
-          <div className="card-content">
-            {
-              fetchingProject ? (
+        <div className="card z-depth-1">
+          {
+            fetchingProject ? (
+              <div className="card-content">
                 <div className="progress">
                   <div className="indeterminate"></div>
-                </div>): ''
-            }
-            <h3 className="card-title">
-              {fetchedProject ? fetchedProject.title : ''}
-            </h3>
-            <p>
-              {fetchedProject ? fetchedProject.content: ''}
-            </p>
-          </div>
-          {/* Author Details*/}
-          <div className="card-action grey lighten-4 grey-text">
-            <div>Posted By The Net Ninja</div>
-            <div>2nd September, 2am</div>
-          </div>
+                </div>
+              </div>
+              ) : (
+              <div>
+                <div className="card-content">
+                  <h3 className="card-title">
+                    {
+                      !fetchingProject && fetchedProject ?
+                      fetchedProject.title : ''
+                    }
+                  </h3>
+                  <p className="card-body">
+                    {
+                      !fetchingProject && fetchedProject ?
+                      fetchedProject.content: ''
+                    }
+                  </p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                  <div>Posted By The Net Ninja</div>
+                  <div>2nd September, 2am</div>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </>
