e818a97445f0da1ad1fe50d7867dba26ded5ded9
Akash <subs@appsparkler.com>
2020-01-15 13:39:04 +0530

    updated webpacks package

--- packages/webpacks/webpack.config.js
+++ packages/webpacks/webpack.config.js
@@ -1,3 +1,6 @@
+
+const DashboardPlugin = require('webpack-dashboard/plugin');
 module.exports = {
-  entry: 'src/index.js',
+  entry: './src/index.js',
+  plugins: [new DashboardPlugin({port: 3001})],
 };
