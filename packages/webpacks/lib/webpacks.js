f8949843c8996f3e7b4d8119d2db75b3c419beef
Aakash Shah <ashah@lowes.com>
2020-01-24 18:32:54 +0530

    understanding nohoist

--- packages/webpacks/lib/webpacks.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = webpacks;
-
-function webpacks() {
-    // TODO
-}
--- packages/webpacks/webpack.config.js
+++
@@ -1,6 +0,0 @@
-
-const DashboardPlugin = require('webpack-dashboard/plugin');
-module.exports = {
-  entry: './src/index.js',
-  plugins: [new DashboardPlugin({port: 3001})],
-};
